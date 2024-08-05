e7435e985b4de4d58a532f63c121964aa691e5fb
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-08-05 14:32:33 +0300

    Adding Backend Folder

---
+++ Frontend/src/Types.ts
@@ -0,0 +1,24 @@
+export type Data = {
+  title: string;
+  description: string;
+  img: string;
+  imgTitle: string;
+  imgThumb: string;
+  imgVertical: string;
+  trailer: string;
+  movie: string;
+  duration: string;
+  year: string;
+  limit: string;
+  genre: string;
+  isSeries: boolean;
+}
+
+export type User = {
+  username: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+  profilePicture: string;
+  myList: Data[];
+}
