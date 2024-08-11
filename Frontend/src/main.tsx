fda36ab85513502551cafcc9f8602e4ed34379ca
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-08-11 10:07:54 +0300

    Revert "Yagelbranch"

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { HelmetProvider } from "react-helmet-async";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
