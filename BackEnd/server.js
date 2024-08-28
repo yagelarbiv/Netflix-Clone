3a4eba98c667fff514de31ac01a40d284c35f5ff
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-08-28 16:07:34 +0300

    **

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // app.use("/api/v1/search", protectRoute, searchRoutes);
 
 //Microservices
-const auth = proxy("http://auth-service:5000")
+const auth = proxy("http://localhost:5000")
 const content = proxy("http://localhost:6000")
 app.use('/api/v2/auth', auth);
 app.use('/api/v2/Content', content);
