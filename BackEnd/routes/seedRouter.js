be415cf4d0ba0fb3cac902881227c1bf67a49890
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-09-01 19:13:49 +0300

    **

--- BackEnd/routes/seedRouter.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import seedData from "../controllers/seedController.js";
-
-const seedRouter = express.Router();
-seedRouter.get("/", seedData);
-
-export default seedRouter;
