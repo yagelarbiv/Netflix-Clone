2832843c4c6c2cace5e88d8ce2c9d4f0de96114c
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-09-02 09:17:12 +0300

    **

---
+++ BackEnd/routes/contentRoute.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getSeasonDetails, getSimilarContent, getTrendingContent, getContentByCategory, getContentDetails, getContentTrailers, getAllContentByCategory } from '../controllers/contentController.js';
+
+const contentRouter = express.Router();
+contentRouter.get('/trending/all', getAllContentByCategory);
+contentRouter.get('/trending/:type', getTrendingContent);
+contentRouter.get('/:id/trailers/:type', getContentTrailers);
+contentRouter.get('/:id/details/:type', getContentDetails);
+contentRouter.get('/:id/similar/:type', getSimilarContent);
+contentRouter.get('/:category/:type', getContentByCategory);
+contentRouter.get('/:id/season/:seasonNumber', getSeasonDetails);
+
+export default contentRouter;
