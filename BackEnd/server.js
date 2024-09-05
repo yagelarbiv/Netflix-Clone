574a942c1faf6232dda50ce523fbc26fadba20aa
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-09-05 11:26:13 +0300

    fixed proxy

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -30,8 +30,8 @@ app.use(cookieParser());
 // app.use("/api/v1/search", protectRoute, searchRoutes);
 
 //Microservices
-const auth = proxy("https://https://netflix-clone-two-eosin.vercel.app")
-const content = proxy("https://https://netflix-clone-content.vercel.app")
+const auth = proxy("https://netflix-clone-two-eosin.vercel.app")
+const content = proxy("https://netflix-clone-content.vercel.app")
 app.use('/api/v2/auth', auth);
 app.use('/api/v2/Content', content);
 
