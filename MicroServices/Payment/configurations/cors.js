392091cb036bfe84246a7f69004bffeb8558967d
yagelarbiv <142971084+yagelarbiv@users.noreply.github.com>
2024-09-27 09:13:24 +0300

    removing the two MicroServices to restructure them

--- MicroServices/Payment/configurations/cors.js
+++
@@ -1,8 +0,0 @@
-export const corsConfiguration = () => {
-  return {
-    origin: ["http://localhost:5173"],
-    credentials: true, //access-control-allow-credentials:true
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  };
-};
